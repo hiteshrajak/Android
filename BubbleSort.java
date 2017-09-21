40fb721d199a45911d0b6d4220616a39a2a42c24
hiteshrajak <31859653+hiteshrajak@users.noreply.github.com>
2017-09-22 01:07:58 +0530

    Linear Search and Sorting

---
+++ BubbleSort.java
@@ -0,0 +1,83 @@
+
+ class BubbleSortExample  {
+
+	private int []arr;
+	int counter;
+	
+	public int [] getArr() {
+		return arr;
+	}
+	public void setArr(int [] arr) {
+		this.arr = arr;
+	}
+	
+	void LinearSearchExample(int item) {
+		int num=arr.length;
+		
+		 for ( counter = 0; counter < num; counter++)
+	      {
+	         if (arr[counter] == item) 
+	         {
+	        	 System.out.println();
+	           System.out.println(item+" is present at location "+(counter+1));
+	           /*Item is found so to stop the search and to come out of the 
+	            * loop use break statement.*/
+	           break;
+	         }
+	      }
+		 
+		//If item is not found
+		 
+		 if (counter == num) {
+	    	  System.out.println();
+	        System.out.println(item + " doesn't exist in array.");
+	      }
+	   }
+	
+	  void bubbleSort() {  
+	        int n = arr.length;  
+	        int temp = 0;  
+	         for(int i=0; i < n; i++){  
+	                 for(int j=1; j < (n-i); j++){  
+	                          if(arr[j-1] > arr[j]){  
+	                                 //swap elements  
+	                                 temp = arr[j-1];  
+	                                 arr[j-1] = arr[j];  
+	                                 arr[j] = temp;  
+	                         }  
+	                          
+	                 }  
+	         }  
+	  
+	    }
+	  
+	  void Display() {
+		  System.out.println();
+		  System.out.println("Array After Bubble Sort");  
+          for(int i=0; i < arr.length; i++){  
+                  System.out.print(arr[i] + " ");  
+          }  
+		  
+	  }
+
+}
+ public class BubbleSort {
+	   public static void main(String[] args) {  
+           int arr[] ={3,60,35,2,45,320,5};  
+            //Display Array Before  Sort
+           System.out.println("Array Before Sort");  
+           for(int i=0; i < arr.length; i++){  
+                   System.out.print(arr[i] + " ");  
+           }  
+           System.out.println();  
+           BubbleSortExample BSE=new BubbleSortExample();
+         
+           BSE.setArr(arr);//Set Array value via setArr
+           BSE.LinearSearchExample(320);//Find Number by Linear Search;
+           BSE.bubbleSort();//Sorting by BubbleShort.
+           BSE.Display();//Display function.
+          
+
+   } 
+	
+}
