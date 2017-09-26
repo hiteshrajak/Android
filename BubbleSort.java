b500c7f0806a8bc9fce2624c53b5a0b5084785ab
hiteshrajak <31859653+hiteshrajak@users.noreply.github.com>
2017-09-26 19:46:08 +0530

    BubbleSort class with binarySearch

--- BubbleSort.java
+++ BubbleSort.java
@@ -51,6 +51,28 @@
 	  
 	    }
 	  
+	int  binarySearch(int arr[], int x){
+		 int l = 0, r = arr.length - 1;
+	        while (l <= r)
+	        {
+	            int m = l + (r-l)/2;
+	 
+	            // Check if x is present at mid
+	            if (arr[m] == x)
+	                return m;
+	 
+	            // If x greater, ignore left half
+	            if (arr[m] < x)
+	                l = m + 1;
+	 
+	            // If x is smaller, ignore right half
+	            else
+	                r = m - 1;
+	        }
+	     // if we reach here, then element was not present
+	        return -1;
+	  }
+	  
 	  void Display() {
 		  System.out.println();
 		  System.out.println("Array After Bubble Sort");  
@@ -65,6 +87,7 @@
 	   public static void main(String[] args) {  
            int arr[] ={3,60,35,2,45,320,5};  
             //Display Array Before  Sort
+           
            System.out.println("Array Before Sort");  
            for(int i=0; i < arr.length; i++){  
                    System.out.print(arr[i] + " ");  
@@ -76,6 +99,12 @@
            BSE.LinearSearchExample(320);//Find Number by Linear Search;
            BSE.bubbleSort();//Sorting by BubbleShort.
            BSE.Display();//Display function.
+           System.out.println(); 
+           int result = BSE.binarySearch(arr, 320);
+           if (result == -1)
+               System.out.println("Element not present");
+           else
+               System.out.println("Element found at index "+result);
           
 
    } 
